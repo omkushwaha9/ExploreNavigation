587c6f61699eb49f1549a1f243f0031ff16c6069
omkushwaha9 <omkushwaha2005@gmail.com>
2024-06-29 18:29:26 +0530

    App

---
+++ Source/Screens/Home.tsx
@@ -0,0 +1,44 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+// navigation
+import {NativeStackScreenProps} from "@react-navigation/native-stack"
+import {RootStackParamList} from '../App'
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+const Home = ({navigation}: HomeProps) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.smallText}>Home Screen</Text>
+      <Button
+      title='Go to details'
+      // onPress={() => navigation.navigate("Details", {
+      //   productId: "86"
+      // })}
+      
+      // onPress={() => navigation.navigate("Details")}
+
+      onPress={() => navigation.push('Details', {
+        productId: "! NO PRODUCTS"
+      })}
+
+      />
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    smallText: {
+        fontSize:24,
+        color: "#grey",
+        fontWeight:"bold",
+    }
+})
